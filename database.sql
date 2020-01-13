e8f627fa71bbe391448413eacef5ab79eaf491a2
Jamie Richison <jamierichi@yahoo.com>
2020-01-13 11:16:41 -0600

    updated database.sql file

--- database.sql
+++ database.sql
@@ -6,5 +6,38 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "email" VARCHAR(80) NOT NULL,
+    "type" VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE "brewery_info"(
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (80) NOT NULL,
+	"bio" VARCHAR (500),
+	"street" VARCHAR(80),
+	"city" VARCHAR (80),
+	"state" VARCHAR (80),
+	"zip" INT,
+	"user_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "favorites" (
+	"user_id" INT REFERENCES "user",
+	"brewery_id" INT REFERENCES "brewery_info",
+    PRIMARY KEY ("user_id", "brewery_id")
+);
+
+CREATE TABLE "comments" (
+	"comment_id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"comment_body" VARCHAR (500),
+	"flag" BOOLEAN 
+);
+
+CREATE TABLE "brewery_image" (
+	"id" SERIAL PRIMARY KEY,
+	"image_url" VARCHAR (200),
+	"brewery_id" INT REFERENCES "brewery_info"
+
 );
